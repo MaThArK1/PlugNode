15cff9dbf8df0407d586f0262f3a705c0796c20f
matheus_barucho <matheus.barucho@tecnospeed.com.br>
2022-05-30 16:09:55 -0300

    Adicionada a rota POST Enviar NFSe

--- src/routes/nfseRoutes.js
+++ src/routes/nfseRoutes.js
@@ -4,6 +4,7 @@ const app = express();
 
 const nfse = express.Router();
 
+//consultar NFSe
 nfse.get("/consulta/:id", async (req, res) => {
   const id = req.params.id;
 
@@ -19,8 +20,29 @@ nfse.get("/consulta/:id", async (req, res) => {
   try {
     const { data } = await axios(config);
     return res.json(data);
-  } catch (response) {
-    res.send(response);
+  } catch (error) {
+    res.send(error.response.data);
+  }
+});
+
+//Enviar NFSe
+nfse.post("/emitir", async (req, res) => {
+  var data = req.body;
+  var config = {
+    method: "post",
+    url: "https://api.sandbox.plugnotas.com.br/nfse",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": "2da392a6-79d2-4304-a8b7-959572c7e44d",
+    },
+    data: data,
+  };
+
+  try {
+    const { data } = await axios(config);
+    return res.json(data);
+  } catch (error) {
+    res.send(error.response.data);
   }
 });
 
